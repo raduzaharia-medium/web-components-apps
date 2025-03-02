6ff84190b9118498b5402c183b1098236168a817
Radu Zaharia <raduzaharia@outlook.com>
2025-03-02 10:33:58 +0200

    Style fixes for album browser.

--- music/components/album-browser.js
+++ music/components/album-browser.js
@@ -1,9 +1,5 @@
-import "./album-list-item.js";
-import "./song-list-item.js";
-
-import "../../shared/components/item-counter.js";
-import "../../shared/components/custom-list.js";
-import "../../shared/components/custom-list-skeleton.js";
+import "./albums-section.js";
+import "./songs-section.js";
 
 import { loadAlbums, loadSongsForArtist } from "../scripts/lists.js";
 import { getFileUrl } from "../scripts/services.js";
@@ -14,51 +10,25 @@ export class AlbumBrowser extends HTMLElement {
 
     history.pushState({ page: "albums" }, "Music - browse by album", ".");
 
-    this.innerHTML = `<section id="albums" class="full-screen has-title has-input">
-      <item-counter id="albumCount" singular="album" plural="albums" order="date added"></item-counter>
-      <input id="albumFilter" type="text" placeholder="search..." />
-      <custom-list id="albumList" class="full-screen">
-        <template slot="item">
-          <album-list-item></album-list-item>
-        </template>
-      </custom-list>
-      <custom-list-skeleton></custom-list-skeleton>
-    </section>
-    <section id="songs" class="full-screen has-title has-subtitle">
-      <item-counter id="songCount" singular="song" plural="songs" order="album"></item-counter>
-      <span id="albumName" class="subtitle"></span>
-      <custom-list id="songList" class="full-screen">
-        <template slot="item">
-          <song-list-item></song-list-item>
-        </template>
-      </custom-list>
-      <custom-list-skeleton></custom-list-skeleton>
-    </section>
-    `;
+    this.innerHTML = `
+      <albums-section id="albums"></albums-section>
+      <songs-section id="songs"></songs-section>`;
 
-    this.querySelector("#albumFilter").addEventListener("keyup", () => {
-      this.querySelector("#albumList").filter(this.querySelector("#albumFilter").value);
+    this.querySelector("albums-section").addEventListener("change", async () => {
+      const selection = this.querySelector("albums-section").selection;
+
+      this.querySelector("#albumName").innerText = selection;
+      await loadSongsForArtist(this.querySelector("artists-section").selection, selection);
     });
-    this.querySelector("#albumList").addEventListener("change", async () => {
-      const selection = this.querySelector("#albumList").selectedData;
 
-      if (selection) {
-        document.querySelector("body").classList.add("album-selected");
-        this.querySelector("#albumName").innerText = selection.item;
-        document.querySelector("selected-item-nav").value = selection.item;
-
-        await loadSongsForArtist(selection.artist, selection.item);
-      }
-    });
-    this.querySelector("#songList").addEventListener("change", async () => {
-      const selection = this.querySelector("#songList").selectedData;
+    this.querySelector("songs-section").addEventListener("change", async () => {
+      const selection = this.querySelector("songs-section").selection;
 
       if (selection) {
         const songs = this.querySelector("#songList").allData;
 
         document.querySelector("audio-player").setPlaylist(songs);
         document.querySelector("audio-player").src = await getFileUrl(selection.file);
-        document.querySelector("body").classList.add("song-selected");
       }
     });
   }
--- music/components/albums-section.js
+++ music/components/albums-section.js
@@ -14,6 +14,7 @@ export class AlbumsSection extends HTMLElement {
 
     this.innerHTML = `
         <item-counter id="albumCount" singular="album" plural="albums" order="date added"></item-counter>
+        <input id="albumFilter" type="text" placeholder="search..." />
         <custom-list id="albumList" class="full-screen">
           <template slot="item">
             <album-list-item></album-list-item>
@@ -21,6 +22,9 @@ export class AlbumsSection extends HTMLElement {
         </custom-list>
         <custom-list-skeleton></custom-list-skeleton>`;
 
+    this.querySelector("custom-list").addEventListener("keyup", (e) => {
+      this.querySelector("custom-list").filter(this.querySelector("input").value);
+    });
     this.querySelector("custom-list").addEventListener("change", async () => {
       const selection = this.querySelector("custom-list").selectedData;
 
