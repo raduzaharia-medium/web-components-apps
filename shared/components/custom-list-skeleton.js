beca598d90dded7aa51e932ef28a2b38ce8ad374
Radu Zaharia <raduzaharia@outlook.com>
2025-03-04 13:46:09 +0200

    More cleanup and refactoring.

--- shared/components/custom-list-skeleton.js
+++ shared/components/custom-list-skeleton.js
@@ -3,35 +3,30 @@ export class CustomListSkeleton extends HTMLElement {
     super();
 
     this.innerHTML = `
-      <div class="loading">
-        <div class="loader-container">
-          <div class="loader-item">
-            <div class="loader small-heading"></div>
-          </div>
-
-          <div class="loader-item">
-            <div class="loader circle"></div>
-            <div class="loader line"></div>
-          </div>
-          <div class="loader-item">
-            <div class="loader circle"></div>
-            <div class="loader line"></div>
-          </div>
-          <div class="loader-item">
-            <div class="loader circle"></div>
-            <div class="loader line"></div>
-          </div>
-          <div class="loader-item">
-            <div class="loader circle"></div>
-            <div class="loader line"></div>
-          </div>
-          <div class="loader-item">
-            <div class="loader circle"></div>
-            <div class="loader line"></div>
-          </div>
-        </div>
+      <div class="loader-item">
+        <div class="loader small-heading"></div>
       </div>
-    `;
+
+      <div class="loader-item">
+        <div class="loader circle"></div>
+        <div class="loader line"></div>
+      </div>
+      <div class="loader-item">
+        <div class="loader circle"></div>
+        <div class="loader line"></div>
+      </div>
+      <div class="loader-item">
+        <div class="loader circle"></div>
+        <div class="loader line"></div>
+      </div>
+      <div class="loader-item">
+        <div class="loader circle"></div>
+        <div class="loader line"></div>
+      </div>
+      <div class="loader-item">
+        <div class="loader circle"></div>
+        <div class="loader line"></div>
+      </div>`;
   }
 }
 
