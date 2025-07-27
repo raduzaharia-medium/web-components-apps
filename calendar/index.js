f0887fa8166c004687da931b7aa6aeaca599f5ad
Radu Zaharia <raduzaharia@outlook.com>
2025-07-27 20:09:05 +0300

    Slight refactoring.

--- calendar/index.js
+++ calendar/index.js
@@ -1,13 +1,17 @@
 import { generatePDF, loadEvents } from "./scripts/services.js";
 
-document.addEventListener("export-pdf", () => {
+document.addEventListener("export-pdf", handleExportPDF);
+document.addEventListener("import-ics", handleImportICS);
+document.addEventListener("date-changed", handleDateChanged);
+document.addEventListener("event-clicked", handleEventClicked);
+
+function handleExportPDF() {
   const year = parseInt(document.querySelector("date-navigator #year")?.value ?? new Date().getFullYear());
   const month = parseInt(document.querySelector("date-navigator #month")?.value ?? new Date().getMonth() + 1);
 
   generatePDF(year, month - 1);
-});
-
-document.addEventListener("import-ics", async () => {
+}
+function handleImportICS() {
   const filePicker = document.createElement("input");
 
   filePicker.type = "file";
@@ -17,13 +21,11 @@ document.addEventListener("import-ics", async () => {
     loadEvents(filePicker.files);
     this.querySelector("main").innerHTML = `<calendar-grid></calendar-grid>`;
   });
-});
-
-document.addEventListener("date-changed", (e) => {
+}
+function handleDateChanged(e) {
   document.querySelector("main").innerHTML = `<calendar-grid data-year="${e.detail.year}" data-month="${e.detail.month}"></calendar-grid>`;
-});
-
-document.addEventListener("event-clicked", (e) => {
+}
+function handleEventClicked(e) {
   document.querySelector("event-editor").dataset.event = JSON.stringify(e.detail);
   document.querySelector("event-editor dialog").showModal();
-});
+}
